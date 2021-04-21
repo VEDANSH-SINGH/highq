543bc1b7efaccbaa9571fabec6fc89092c856d99
VEDANSH-SINGH <F20190541@pilani.bits-pilani.ac.in.com>
2021-04-21 21:43:56 +0530

    reverted changes in last commit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "highq/build")));
   // Handle React routing, return all requests to React app
-  app.get("/", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "highq/build", "index.html"));
   });
 }
