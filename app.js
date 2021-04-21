a47bf7ba06f9a0c211a623fc36d331a9c8360cd4
VEDANSH-SINGH <F20190541@pilani.bits-pilani.ac.in.com>
2021-04-21 21:55:23 +0530

    serverchnaged

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "highq/build")));
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "highq/build", "index.html"));
+    res.sendFile(path.join(__dirname, "/highq/build", "index.html"));
   });
 }
 const PORT = process.env.PORT || 8080;
